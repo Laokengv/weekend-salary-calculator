2af554051fb83e8adc6a53e1bd06474574fa6d6c
Laokeng Vang <laokengv@gmail.com>
2023-05-13 01:33:11 -0500

    Third commit. Added some JS functions and started adding tables onto HTML side.

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,10 @@
 console.log('Hello World');
 
 function salarySubmit(event) {
+    event.preventDefault();
     console.log(event);
 };
+
+function deleteEntry(event) {
+    event.target.parentElement.parentElement.remove();
+}
