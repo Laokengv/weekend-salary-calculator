439a31cf0c7af648f775a5dda8e0d36c1b60d586
Laokeng Vang <laokengv@gmail.com>
2023-05-24 23:13:25 -0500

    Added catch to get function.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,68 +3,70 @@ console.log('Hello World');
 let contentDiv = document.querySelector('#employeeTable')
 
 function getEmployees() {
-    fetch('/employees')
+  fetch('/employees')
     .then((response) => {
-        return response.json();
+      return response.json();
     })
     .then((employees) => {
-        employeeInfo.innerHTML = employees
-        .map((employee) => {
-            return `
-            <tr>
-                <th>${firstName.firstName}</th>
-                <th>${lastName.lastName}</th>
-                <th>${id.id}</th>
-                <th>${title.title}</th>   
-                <th>${annualSalary.annualSalary}</th>
-            </tr>
-            `;
-        })
-        .join('');
-    });
+      for (let employee of employees) {
+        contentDiv.innerHTML += `
+           <tr>
+               <th>${firstName.firstName}</th>
+               <th>${lastName.lastName}</th>
+               <th>${id.id}</th>
+              <th>${title.title}</th>   
+              <th>${annualSalary.annualSalary}</th>
+           </tr>
+        `;
+      }
+    }).catch((error) => {
+      console.log('Error with request:', error);
+      alert('Something went wrong');
+    })
 };
 getEmployees();
 
 
 function salarySubmit(event) {
-    event.preventDefault();
-    console.log(event);
+  event.preventDefault();
+  console.log(event);
 
-    let firstName = document.querySelector('#first-name').value;
-    let lastName = document.querySelector('#last-name').value;
-    let idNumber = document.querySelector('#id-number').value;
-    let jobTitle = document.querySelector('#job-title').value;
-    let annualSalary = document.querySelector('#annual-salary').value;
-    console.log('Inputs:', firstName, lastName, idNumber, jobTitle, annualSalary);
+  let firstName = document.querySelector('#first-name').value;
+  let lastName = document.querySelector('#last-name').value;
+  let idNumber = document.querySelector('#id-number').value;
+  let jobTitle = document.querySelector('#job-title').value;
+  let annualSalary = document.querySelector('#annual-salary').value;
+  console.log('Inputs:', firstName, lastName, idNumber, jobTitle, annualSalary);
 
-    let employeeToAdd = JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        idNumber: idNumber,
-        jobTitle: jobTitle,
-        annualSalary: annualSalary
-    });
+  let employeeToAdd = JSON.stringify({
+    firstName: firstName,
+    lastName: lastName,
+    idNumber: idNumber,
+    jobTitle: jobTitle,
+    annualSalary: annualSalary
+  });
 
-    fetch('/employees', {
-        method: 'POST',
-        body: employeeToAdd,
-        headers: { 'Content-Type': 'application/json'}})
-    .then((response) =>  {
-        console.log('POST response', response);
+  fetch('/employees', {
+    method: 'POST',
+    body: employeeToAdd,
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((response) => {
+      console.log('POST response', response);
 
-        document.querySelector('#first-name').value = '';
-        document.querySelector('#last-name').value = '';
-        document.querySelector('#id-number').value = '';
-        document.querySelector('#fjob-title').value = '';
-        document.querySelector('#annual-salary').value = '';
+      document.querySelector('#first-name').value = '';
+      document.querySelector('#last-name').value = '';
+      document.querySelector('#id-number').value = '';
+      document.querySelector('#fjob-title').value = '';
+      document.querySelector('#annual-salary').value = '';
 
-        getEmployees();
-        }).catch((error) => {
-            console.log(error);
-            alert('Something went wrong');
-        })
+      getEmployees();
+    }).catch((error) => {
+      console.log(error);
+      alert('Something went wrong');
+    })
 };
 
 function deleteEntry(event) {
-    event.target.parentElement.parentElement.remove();
+  event.target.parentElement.parentElement.remove();
 };
