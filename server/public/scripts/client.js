88d79f6b1c7e15c9824b161c736e2678545a324c
Laokeng Vang <laokengv@gmail.com>
2023-05-30 18:01:04 -0500

    added innerHTML to salarySubmit function. Added color change to red. Working on CSS now.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,38 +6,38 @@ let totalSalary = 0;
 
 const salary = document.querySelector('#monthlyTotal');
 
-function getEmployees() {
-  console.log('in getEmployees');
-  // function to get employees from server
-  fetch('/employees')
-    .then((response) => {
-      console.log('Response received', response);
-      return response.json();
-    })
-    .then((employees) => {
-      let table = document.getElementById('#employeeTable')
-      let contentDiv = document.querySelector('#employeeTable')
-      console.log(employees)
+// function getEmployees() {
+//   console.log('in getEmployees');
+//   // function to get employees from server
+//   fetch('/employees')
+//     .then((response) => {
+//       console.log('Response received', response);
+//       return response.json();
+//     })
+//     .then((employees) => {
+//       let table = document.getElementById('#employeeTable')
+//       let contentDiv = document.querySelector('#employeeTable')
+//       console.log(employees)
 
-      contentDiv.innerHTML = '';
-      for (let employee of employees) {
-        contentDiv.innerHTML += `
-           <tr>
-              <th>${employee.firstName}</th>
-              <th>${employee.lastName}</th>
-              <th>${employee.idNumber}</th>
-              <th>${employee.jobTitle}</th>   
-              <th>${employee.annualSalary}</th>
-              <th><button onclick="deleteEntry(event)">Delete</th>
-           </tr>
-        `;
-      }
-    }).catch((error) => {
-      console.log('Error with request:', error);
-      alert('Something went wrong');
-    })
-};
-getEmployees();
+//       contentDiv.innerHTML = '';
+//       for (let employee of employees) {
+//         contentDiv.innerHTML += `
+//            <tr>
+//               <th>${employee.firstName}</th>
+//               <th>${employee.lastName}</th>
+//               <th>${employee.idNumber}</th>
+//               <th>${employee.jobTitle}</th>   
+//               <th>${employee.annualSalary}</th>
+//               <th><button onclick="deleteEntry(event)">Delete</th>
+//            </tr>
+//         `;
+//       }
+//     }).catch((error) => {
+//       console.log('Error with request:', error);
+//       alert('Something went wrong');
+//     })
+// };
+// getEmployees();
 
 
 function salarySubmit(event) {
@@ -49,36 +49,56 @@ function salarySubmit(event) {
   let lastName = document.querySelector('#last-name').value;
   let idNumber = document.querySelector('#id-number').value;
   let jobTitle = document.querySelector('#job-title').value;
-  let annualSalary = Number(document.querySelector('#annual-salary').value);
-  console.log('Inputs:', firstName, lastName, idNumber, jobTitle, annualSalary);
+  let annualSalary = Number(document.querySelector('#annual-salary').value); // the Number() function changes this from 
+  // a string to a point number
+  // https://www.w3docs.com/snippets/javascript/how-to-convert-string-to-number-in-javascript.html
+  let salaryTable = document.querySelector('#total');
+  salaryTable.innerHTML += `
+    <tr>
+      <td>${firstName}</td>
+      <td>${lastName}</td>
+      <td>${idNumber}</td>
+      <td>${jobTitle}</td>
+      <td>${annualSalary}</td>
+      <td><button onclick="deleteEntry(event)">Delete</td>
+    </tr>
+  `;
+  salaryArray.push(annualSalary);
+  totalSalary += annualSalary;
+  salary.innerText = totalSalary;
 
-  let employeeToAdd = JSON.stringify({
-    firstName: firstName,
-    lastName: lastName,
-    idNumber: idNumber,
-    jobTitle: jobTitle,
-    annualSalary: annualSalary
-  });
+  let changeSalaryColor = document.querySelector('.monthlyTotalSal');
+  if(totalSalary > 20000) {
+    changeSalaryColor.style.backgroundColor = 'red';
+  }
 
-  fetch('/employees', {
-    method: 'POST',
-    body: employeeToAdd,
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then((response) => {
-      console.log('POST response', response);
+  // let employeeToAdd = JSON.stringify({
+  //   firstName: firstName,
+  //   lastName: lastName,
+  //   idNumber: idNumber,
+  //   jobTitle: jobTitle,
+  //   annualSalary: annualSalary
+  // });
 
-      document.querySelector('#first-name').value = '';
-      document.querySelector('#last-name').value = '';
-      document.querySelector('#id-number').value = '';
-      document.querySelector('#job-title').value = '';
-      document.querySelector('#annual-salary').value = '';
+  // fetch('/employees', {
+  //   method: 'POST',
+  //   body: employeeToAdd,
+  //   headers: { 'Content-Type': 'application/json' }
+  // })
+  //   .then((response) => {
+  //     console.log('POST response', response);
 
-      getEmployees();
-    }).catch((error) => {
-      console.log(error);
-      alert('Something went wrong');
-    })
+  //     document.querySelector('#first-name').value = '';
+  //     document.querySelector('#last-name').value = '';
+  //     document.querySelector('#id-number').value = '';
+  //     document.querySelector('#job-title').value = '';
+  //     document.querySelector('#annual-salary').value = '';
+
+  //     getEmployees();
+  //   }).catch((error) => {
+  //     console.log(error);
+  //     alert('Something went wrong');
+  //   })
 };
 
 function deleteEntry(event) {
--- server/server.js
+++ server/server.js
@@ -4,39 +4,39 @@ const PORT = 8000;
 app.use(express.static('server/public'));
 app.use(express.json());
 
-const employeeList = [
-    {
-        firstName: 'Eugene',
-        lastName: 'Krabs',
-        idNumber:  7972,
-        jobTitle: 'Manager',
-        annualSalary: ('$' + 60000)
-    },
-    {
-        firstName: 'Spongebob',
-        lastName: 'Squarepants',
-        idNumber:  7877,
-        jobTitle: 'Cook',
-        annualSalary: ('$' + 25000)
-    },
-    {
-        firstName: 'Squidward',
-        lastName: 'Tentacles',
-        idNumber:  7940,
-        jobTitle: 'Cashier',
-        annualSalary: ('$' + 20000)
-    }
-];
+// const employeeList = [
+//     {
+//         firstName: 'Eugene',
+//         lastName: 'Krabs',
+//         idNumber:  7972,
+//         jobTitle: 'Manager',
+//         annualSalary: ('$' + 60000)
+//     },
+//     {
+//         firstName: 'Spongebob',
+//         lastName: 'Squarepants',
+//         idNumber:  7877,
+//         jobTitle: 'Cook',
+//         annualSalary: ('$' + 25000)
+//     },
+//     {
+//         firstName: 'Squidward',
+//         lastName: 'Tentacles',
+//         idNumber:  7940,
+//         jobTitle: 'Cashier',
+//         annualSalary: ('$' + 20000)
+//     }
+// ];
                     
 
-app.get('/employees', (req, res) => {
-    res.send(employeeList);
-});
+// app.get('/employees', (req, res) => {
+//     res.send(employeeList);
+// });
 
-app.post('/employees', (req, res) => {
-    employeeList.push(req.body);
-    res.sendStatus(201);
-})
+// app.post('/employees', (req, res) => {
+//     employeeList.push(req.body);
+//     res.sendStatus(201);
+// })
 
 app.listen(PORT, () => {
     console.log('Server running on:', PORT);
